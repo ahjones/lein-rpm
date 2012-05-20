ffb295425ee635eec6c026d1459d85b53ba5a6e3
Andrew Jones <me@andrewhjon.es>
2012-05-20 20:24:50 +0100

    Rename plugin

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject plugin "0.0.1-SNAPSHOT"
+(defproject rpm "0.0.1-SNAPSHOT"
   :description "Create an RPM"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.codehaus.mojo/rpm-maven-plugin "2.1-alpha-1"]])
